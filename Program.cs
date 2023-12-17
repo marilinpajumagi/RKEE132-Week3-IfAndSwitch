c911b3f40fff75a4f4093ef30c5364d6b3eaee85
Marilin Pajumägi <marilin.pajumagi@tktk.ee>
2023-12-17 15:57:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//modulus - %
+
+int a = 24;
+int b = 8;
+
+int c = a % b; //+/-/* - /
+
+Console.WriteLine(c);
